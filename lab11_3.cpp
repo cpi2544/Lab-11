08d31a7e0ba16f5ac5b715373a1f7b296026fce1
Your NameCpi2544 <pokpong1789@gmail.com>
2021-12-17 16:34:27 +0700

    lab11_3.cpp completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,30 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+
+    ifstream source("score.txt");
+    string t;
+    int n = 0;
+    double sum=0;
+    double Z=0;
+
+    while(getline(source,t)){
+        sum += atof(t.c_str());
+        Z += pow(atof(t.c_str()),2);
+        n++;
+    }
+
+    double M = sum/n;
+    double S = sqrt((Z/n)-pow(M,2));
+
+
+    cout << "Number of data = " << n << "\n";
+    cout << setprecision(3);
+    cout << "Mean = " << M << "\n";
+    cout << "Standard deviation = " << S;
+}
